cc6428df2ca222c7c9c483888349bf1bf3b4e387
Teena Thakur <teenathakur4050@gmail.com>
2023-08-10 00:14:38 +0530

    Add files via upload

---
+++ guessthenumber/GuessTheNumber.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guessthenumber;
+
+/**
+ *
+ * @author TEENA THAKUR
+ */
+public class GuessTheNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int num=(int)Math.random();
+        new NewJFrame().setVisible(true);
+        
+    }
+    
+}
